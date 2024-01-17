d207653bd270c6a666f65bef283abd975fc537d0
Sean Bailey <seanbailey15@gmail.com>
2024-01-16 20:37:11 -0500

    worked on markov.js, seems to work, needs further testing

--- markov.js
+++ markov.js
@@ -1,14 +1,22 @@
 /** Textual markov chain generator */
+const fs = require("fs");
 
+let file;
+
+try {
+  file = fs.readFileSync("eggs.txt", "utf8");
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.words = words.filter((c) => c !== "");
+    this.chains = this.makeChains(this.words);
   }
 
   /** set markov chains:
@@ -16,14 +24,50 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // TODO
+  makeChains(words) {
+    let chains = {};
+    let lastWord = "";
+    words.forEach((word) => {
+      if (!chains.hasOwnProperty(word)) {
+        chains[word] = [];
+        if (lastWord === "") {
+          lastWord = word;
+        } else {
+          chains[lastWord].push(word);
+          lastWord = word;
+        }
+      } else if (chains.hasOwnProperty(word)) {
+        chains[lastWord].push(word);
+        lastWord = word;
+      }
+    });
+    chains[lastWord].push(null);
+    return chains;
   }
-
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
-    // TODO
+  makeText(chains = this.chains, numWords = 100) {
+    let keys = Object.keys(chains);
+    let start = keys[Math.floor(Math.random() * keys.length)];
+    let result = start;
+    let current = start;
+    for (let i = 2; i <= numWords; i++) {
+      let next =
+        chains[current][Math.floor(Math.random() * chains[current].length)];
+      if (next == null) {
+        console.log(result);
+        return;
+      }
+      result += " " + next;
+      current = next;
+    }
+    console.log(result);
   }
 }
+
+const m = new MarkovMachine(file);
+// const m = new MarkovMachine("the cat in the hat is in the hat");
+console.log(m);
+
+m.makeText(m.chains, 200);
